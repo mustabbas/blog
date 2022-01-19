0009f589b07603ab2350e980d4cbed28deb37ba6
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-01-19 21:58:18 +0300

    Blog app - creating a data model

---
+++ db/migrate/20220119182756_create_commits.rb
@@ -0,0 +1,11 @@
+class CreateCommits < ActiveRecord::Migration[7.0]
+  def change
+    create_table :commits do |t|
+      t.text :text
+      t.references :author, null: false, foreign_key: {to_table: :users},index: true
+      t.references :post, null: false, foreign_key: true, index: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_164033) do
+ActiveRecord::Schema.define(version: 2022_01_19_173715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string "name"
