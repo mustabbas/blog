ea23bf4be35018e023304d18d4e70741a2b7fc71
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-01-24 22:52:25 +0300

    Blog App - processing data in models

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,4 +1,8 @@
 class Commit < ApplicationRecord
-  belongs_to :author
+  belongs_to :user
   belongs_to :post
+  def update_comments_count(post_id)
+    post = Post.find_by(post_id)
+    post.update(comments_counter: comments_counter+1)
+  end
 end
