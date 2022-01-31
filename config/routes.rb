7942e897d02dfe64202f63a16c0b493678b1c63e
DyaryRaoof <dyary2020@gmail.com>
2022-01-31 23:54:33 +0300

    Fix reset passowrd redirect error with devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root "users#index"
   root 'users#index'
-  get "/user", to: "users#index"
+  get "/users", to: "users#index"
   get "/users/:id", to: "users#show", as: "user_show"
   get "/users/:id/posts", to: "posts#index", as: "all_posts" 
   get "/users/:user_id/posts/:post_id", to: "posts#show", as: "posts_show"
