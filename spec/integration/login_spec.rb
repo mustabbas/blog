d97b90cb52e74766b2c7d33d726a590710bd2a5f
DyaryRaoof <dyary2020@gmail.com>
2022-02-02 21:19:29 +0300

    Finish Login_spec tests

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Login page', type: :system do
   describe 'login page' do
     before(:each) do
-      User.create(name: 'Luis', email: 'luis@gmail.com', password: '123456', photo: 'image1',
-                  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nulla aliquet et lorem at volutpat. Aenean vel justo diam.
-                  Vivamus ultricies vulputate dolor, et molestie turpis convallis et.
-                  In sed metus.', posts_counter: 0)
-      visit new_user_session_path :host => 'localhost:3000'
+      visit new_user_session_path
     end
 
     it 'Can see the user name input' do
@@ -31,16 +26,17 @@ RSpec.describe 'Login page', type: :system do
 
     it 'When I click the submit button after filling in the username and the password with incorrect data,
     I get a detailed error.' do
-      fill_in 'Username', with: 'user@example.com'
-      fill_in 'Password', with: 'password'
+      fill_in 'username', with: 'user@example.com'
+      fill_in 'password', with: 'password'
       click_button 'Log in'
       expect(page).to have_content('Invalid Email or password.')
     end
 
     it 'When I click the submit button after filling in the username and the password with correct data,
     I am redirected to the root page.' do
-      fill_in 'Username', with: 'luis@gmail.com'
-      fill_in 'Password', with: '123456'
+      visit new_user_session_path
+      fill_in 'username', with: 'abbas@gmail.com'
+      fill_in 'password', with: '123456'
       click_button 'Log in'
       expect(page).to have_current_path(root_path)
     end
