397452704595d8de048d62c21a83b0b57e1172b0
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-03 18:32:25 +0300

    Create an API endpoint to add a comment to a post.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+  
   def index
     @comments = Comment.where(post_id: params[:post_id])
     respond_to do |format|
@@ -22,15 +22,18 @@ class CommentsController < ApplicationController
     comment.user_id = params[:user_id]
     comment.post = post
     respond_to do |format|
-      format.html do
         if comment.save
           Comment.update_comments_counter(params[:id])
+          format.json { render json: { message: 'You have successfully created a comment' } }
+          format.html do
           flash[:success] = 'Comment saved successfully'
           redirect_to all_posts_path(params[:user_id])
+          end
         else
+          
+          format.json { render json: { message: "You have An Error #{:error}" } }
           flash.now[:error] = 'Error: Comment could not be saved'
           render :new, locals: { comment: comment }
-        end
       end
     end
   end
